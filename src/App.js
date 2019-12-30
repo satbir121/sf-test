6445e7caafc5c6968c1a1c4826982af58738df4b
Satbir <satbir@Satbirs-MacBook-Pro.local>
2019-12-30 17:42:41 +0530

    Adding Grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent
+} from "@syncfusion/ej2-react-grids";
+import { data } from "./datasource";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GridComponent dataSource={data}>
+        <ColumnsDirective>
+          <ColumnDirective field="OrderID" width="100" textAlign="Right" />
+          <ColumnDirective field="CustomerID" width="100" />
+          <ColumnDirective field="EmployeeID" width="100" textAlign="Right" />
+          <ColumnDirective
+            field="Freight"
+            width="100"
+            format="C2"
+            textAlign="Right"
+          />
+          <ColumnDirective field="ShipCountry" width="100" />
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   );
 }
