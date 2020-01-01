ea2ab9f1d0e5d4abd31a57cbc63fa1a7ce420e09
Satbir <satbir@Satbirs-MacBook-Pro.local>
2020-01-01 13:05:55 +0530

    Adding toggle width height.

--- src/App.js
+++ src/App.js
@@ -9,28 +9,56 @@ import {
 } from "@syncfusion/ej2-react-grids";
 import { data } from "./datasource";
 
-function App() {
-  return (
-    <div className="App">
-      <div className={"gridContainer"}>
-        <GridComponent dataSource={data}>
-          <Inject services={[Page]} />
-          <ColumnsDirective>
-            <ColumnDirective field="OrderID" width="100" textAlign="Right" />
-            <ColumnDirective field="CustomerID" width="100" />
-            <ColumnDirective field="EmployeeID" width="100" textAlign="Right" />
-            <ColumnDirective
-              field="Freight"
-              width="100"
-              format="C2"
-              textAlign="Right"
-            />
-            <ColumnDirective field="ShipCountry" width="100" />
-          </ColumnsDirective>
-        </GridComponent>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullWidth: true,
+      height: 1000
+    };
+  }
+  toggleWidth = () => {
+    this.setState({
+      fullWidth: !this.state.fullWidth
+    });
+  };
+  toggleHeight = () => {
+    this.setState({
+      height: this.state.height === 1000 ? 300 : 1000
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.toggleHeight}>Resize</button>
+        <div className={this.state.fullWidth ? "full-width" : "half-width"}>
+          <GridComponent
+            dataSource={data}
+            height={this.state.height}
+            allowPaging={true}
+          >
+            <Inject services={[Page]} />
+            <ColumnsDirective>
+              <ColumnDirective field="OrderID" width="100" textAlign="Right" />
+              <ColumnDirective field="CustomerID" width="100" />
+              <ColumnDirective
+                field="EmployeeID"
+                width="100"
+                textAlign="Right"
+              />
+              <ColumnDirective
+                field="Freight"
+                width="100"
+                format="C2"
+                textAlign="Right"
+              />
+              <ColumnDirective field="ShipCountry" width="100" />
+            </ColumnsDirective>
+          </GridComponent>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
