65bbf8fd5066e1fe8351da2504a58cdde981e7c8
Satbir <satbir@Satbirs-MacBook-Pro.local>
2020-01-01 12:54:29 +0530

    Adding pagination.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,32 @@ import "./App.css";
 import {
   ColumnDirective,
   ColumnsDirective,
-  GridComponent
+  GridComponent,
+  Page,
+  Inject
 } from "@syncfusion/ej2-react-grids";
 import { data } from "./datasource";
 
 function App() {
   return (
     <div className="App">
-      <GridComponent dataSource={data}>
-        <ColumnsDirective>
-          <ColumnDirective field="OrderID" width="100" textAlign="Right" />
-          <ColumnDirective field="CustomerID" width="100" />
-          <ColumnDirective field="EmployeeID" width="100" textAlign="Right" />
-          <ColumnDirective
-            field="Freight"
-            width="100"
-            format="C2"
-            textAlign="Right"
-          />
-          <ColumnDirective field="ShipCountry" width="100" />
-        </ColumnsDirective>
-      </GridComponent>
+      <div className={"gridContainer"}>
+        <GridComponent dataSource={data}>
+          <Inject services={[Page]} />
+          <ColumnsDirective>
+            <ColumnDirective field="OrderID" width="100" textAlign="Right" />
+            <ColumnDirective field="CustomerID" width="100" />
+            <ColumnDirective field="EmployeeID" width="100" textAlign="Right" />
+            <ColumnDirective
+              field="Freight"
+              width="100"
+              format="C2"
+              textAlign="Right"
+            />
+            <ColumnDirective field="ShipCountry" width="100" />
+          </ColumnsDirective>
+        </GridComponent>
+      </div>
     </div>
   );
 }
